9d02b78802f5c38ab4d9b4ff692d96f1f31c0cf5
Prince Verma <80540136+PRINCE-67@users.noreply.github.com>
2023-10-27 10:06:19 +0530

    Add files via upload

---
+++ EasMyStay/src/main/java/com/fil/easmystay/EasMyStayApplication.java
@@ -0,0 +1,13 @@
+package com.fil.easmystay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasMyStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasMyStayApplication.class, args);
+	}
+
+}
---
+++ EasMyStay/src/test/java/com/fil/easmystay/EasMyStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil.easmystay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasMyStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
